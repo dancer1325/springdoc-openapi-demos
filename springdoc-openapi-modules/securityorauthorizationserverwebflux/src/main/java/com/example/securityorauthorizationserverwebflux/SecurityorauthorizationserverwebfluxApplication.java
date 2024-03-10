7854256bc2ee8233e55f6ceef2885e3d720bc90b
alfredo-toledano <alfredo.toledano@allianzdirect.es>
2024-03-10 12:08:14 +0100

    doc(modules.seucurityOrAuthServerWebflux): add skeleton

---
+++ springdoc-openapi-modules/securityorauthorizationserverwebflux/src/main/java/com/example/securityorauthorizationserverwebflux/SecurityorauthorizationserverwebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityorauthorizationserverwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityorauthorizationserverwebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityorauthorizationserverwebfluxApplication.class, args);
+	}
+
+}
---
+++ springdoc-openapi-modules/securityorauthorizationserverwebflux/src/test/java/com/example/securityorauthorizationserverwebflux/SecurityorauthorizationserverwebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityorauthorizationserverwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityorauthorizationserverwebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
